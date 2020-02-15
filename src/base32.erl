d5500a827962e49f6e5b955525da960e712a30f7
SUZUKI Tetsuya <szktty@shiguredo.jp>
2020-02-15 17:18:04 +0900

    RFC4648 のデコード処理

--- src/base32.erl
+++ src/base32.erl
@@ -1,6 +1,6 @@
 -module(base32).
 
--export([encode/2, encode_to_string/2]).
+-export([encode/2, encode_to_string/2, decode/2, decode_to_string/2]).
 
 -type ascii_string() :: [1..255].
 -type ascii_binary() :: binary().
@@ -189,4 +189,164 @@ encode_rfc4648_to_string0(<<1:1, Next/bitstring>>, N, Accu) ->
 encode_rfc4648_to_string0(_, _, _) ->
     {error, {fatal, rfc4648}}.
 
+-spec decode(base32_format(), base32_data()) ->
+    {ok, ascii_binary()} | {error, atom()}.
+decode(Format, Data) when is_list(Data) ->
+    decode(Format, list_to_binary(Data));
+decode(rfc4648, Data) ->
+    {ok, decode_rfc4648_0(Data, [])};
+decode(Format, _) ->
+    {error, {invalid_format, Format}}.
+
+-spec decode_to_string(base32_format(), base32_data()) ->
+    {ok, ascii_string()} | {error, atom()}.
+decode_to_string(Format, Data) ->
+    case decode(Format, Data) of
+        {ok, Decoded} ->
+            {ok, binary_to_list(Decoded)};
+        Error ->
+            Error
+    end.
+
+decode_rfc4648_0(<<>>, Accu) ->
+    rev_bits_list_to_binary(Accu);
+decode_rfc4648_0(<<"A", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<0:5>>|Accu]);
+decode_rfc4648_0(<<"B", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<1:5>>|Accu]);
+decode_rfc4648_0(<<"C", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<2:5>>|Accu]);
+decode_rfc4648_0(<<"D", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<3:5>>|Accu]);
+decode_rfc4648_0(<<"E", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<4:5>>|Accu]);
+decode_rfc4648_0(<<"F", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<5:5>>|Accu]);
+decode_rfc4648_0(<<"G", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<6:5>>|Accu]);
+decode_rfc4648_0(<<"H", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<7:5>>|Accu]);
+decode_rfc4648_0(<<"I", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<8:5>>|Accu]);
+decode_rfc4648_0(<<"J", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<9:5>>|Accu]);
+decode_rfc4648_0(<<"K", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<10:5>>|Accu]);
+decode_rfc4648_0(<<"L", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<11:5>>|Accu]);
+decode_rfc4648_0(<<"M", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<12:5>>|Accu]);
+decode_rfc4648_0(<<"N", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<13:5>>|Accu]);
+decode_rfc4648_0(<<"O", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<14:5>>|Accu]);
+decode_rfc4648_0(<<"P", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<15:5>>|Accu]);
+decode_rfc4648_0(<<"Q", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<16:5>>|Accu]);
+decode_rfc4648_0(<<"R", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<17:5>>|Accu]);
+decode_rfc4648_0(<<"S", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<18:5>>|Accu]);
+decode_rfc4648_0(<<"T", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<19:5>>|Accu]);
+decode_rfc4648_0(<<"U", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<20:5>>|Accu]);
+decode_rfc4648_0(<<"V", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<21:5>>|Accu]);
+decode_rfc4648_0(<<"W", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<22:5>>|Accu]);
+decode_rfc4648_0(<<"X", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<23:5>>|Accu]);
+decode_rfc4648_0(<<"Y", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<24:5>>|Accu]);
+decode_rfc4648_0(<<"Z", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<25:5>>|Accu]);
+decode_rfc4648_0(<<"2", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<26:5>>|Accu]);
+decode_rfc4648_0(<<"3", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<27:5>>|Accu]);
+decode_rfc4648_0(<<"4", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<28:5>>|Accu]);
+decode_rfc4648_0(<<"5", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<29:5>>|Accu]);
+decode_rfc4648_0(<<"6", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<30:5>>|Accu]);
+decode_rfc4648_0(<<"7", Next/bitstring>>, Accu) ->
+    decode_rfc4648_0(Next, [<<31:5>>|Accu]);
+
+decode_rfc4648_0(<<"======", Next/bitstring>>, [<<0:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<0:3>>|Accu]);
+decode_rfc4648_0(<<"======", Next/bitstring>>, [<<4:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<1:3>>|Accu]);
+decode_rfc4648_0(<<"======", Next/bitstring>>, [<<8:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<2:3>>|Accu]);
+decode_rfc4648_0(<<"======", Next/bitstring>>, [<<12:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<3:3>>|Accu]);
+decode_rfc4648_0(<<"======", Next/bitstring>>, [<<16:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<4:3>>|Accu]);
+decode_rfc4648_0(<<"======", Next/bitstring>>, [<<20:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<5:3>>|Accu]);
+decode_rfc4648_0(<<"======", Next/bitstring>>, [<<24:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<6:3>>|Accu]);
+decode_rfc4648_0(<<"======", Next/bitstring>>, [<<28:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<7:3>>|Accu]);
+
+decode_rfc4648_0(<<"====", Next/bitstring>>, [<<0:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<0:1>>|Accu]);
+decode_rfc4648_0(<<"====", Next/bitstring>>, [<<16:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<1:1>>|Accu]);
+
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<0:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<0:4>>|Accu]);
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<2:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<1:4>>|Accu]);
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<4:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<2:4>>|Accu]);
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<6:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<3:4>>|Accu]);
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<8:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<4:4>>|Accu]);
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<10:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<5:4>>|Accu]);
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<12:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<6:4>>|Accu]);
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<14:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<7:4>>|Accu]);
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<16:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<8:4>>|Accu]);
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<18:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<9:4>>|Accu]);
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<20:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<10:4>>|Accu]);
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<22:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<11:4>>|Accu]);
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<24:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<12:4>>|Accu]);
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<26:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<13:4>>|Accu]);
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<28:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<14:4>>|Accu]);
+decode_rfc4648_0(<<"===", Next/bitstring>>, [<<30:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<15:4>>|Accu]);
+
+decode_rfc4648_0(<<"=", Next/bitstring>>, [<<0:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<0:2>>|Accu]);
+decode_rfc4648_0(<<"=", Next/bitstring>>, [<<8:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<1:2>>|Accu]);
+decode_rfc4648_0(<<"=", Next/bitstring>>, [<<16:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<2:2>>|Accu]);
+decode_rfc4648_0(<<"=", Next/bitstring>>, [<<24:5>>|Accu]) ->
+    decode_rfc4648_0(Next, [<<3:2>>|Accu]);
+
+decode_rfc4648_0(_, _) ->
+    {error, {fatal, rfc4648}}.
+
+rev_bits_list_to_binary(List) ->
+    rev_bits_list_to_binary0(List, <<>>).
+
+rev_bits_list_to_binary0([], Accu) ->
+    Accu;
+rev_bits_list_to_binary0([Bits|Next], Accu) ->
+    rev_bits_list_to_binary0(Next, <<Bits/bitstring, Accu/bitstring>>).
 
--- test/base32_test.erl
+++ test/base32_test.erl
@@ -2,7 +2,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--import(base32, [encode_to_string/2]).
+-import(base32, [encode_to_string/2, decode_to_string/2]).
 
 rfc4648_encode_test() ->
     Format = rfc4648,
@@ -12,3 +12,13 @@ rfc4648_encode_test() ->
     ?assertEqual({ok, "MZXW6YQ="}, encode_to_string(Format, "foob")),
     ?assertEqual({ok, "MZXW6YTB"}, encode_to_string(Format, "fooba")),
     ?assertEqual({ok, "MZXW6YTBOI======"}, encode_to_string(Format, "foobar")).
+
+rfc4648_decode_test() ->
+    Format = rfc4648,
+    ?assertEqual({ok, "f"}, decode_to_string(Format, "MY======")),
+    ?assertEqual({ok, "fo"}, decode_to_string(Format, "MZXQ====")),
+    ?assertEqual({ok, "foo"}, decode_to_string(Format, "MZXW6===")),
+    ?assertEqual({ok, "foob"}, decode_to_string(Format, "MZXW6YQ=")),
+    ?assertEqual({ok, "fooba"}, decode_to_string(Format, "MZXW6YTB")),
+    ?assertEqual({ok, "foobar"}, decode_to_string(Format, "MZXW6YTBOI======")).
+
